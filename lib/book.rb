227e6bed0481ef9330cb0b21b722e5091dc52282
Sami  Sabeeh <samsabeeh6@gmail.com>
2020-07-07 00:55:27 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,17 +1,20 @@
 class Book
-  attr_accessor :author, :page_count
-  attr_reader :title, :genre 
-
+  attr_accessor :author, :page_count  # remove the attr_accessor for genre
+  attr_reader :title, :genre  # add an attr_reader for genre
+ 
+  GENRES = []
+ 
   def initialize(title)
     @title = title
   end
-  GENRES = []
-  
+ 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
-  def genre=(genre_given)
-    @genre = genre 
+ 
+  # create the writer for genre and add the logic for the class constant
+  def genre=(genre)
+    @genre = genre
     GENRES << genre 
   end
 end
